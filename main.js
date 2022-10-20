91ccc1714a18f72c3f49567293835351879a7943
renan <renan.rodriguesmoura2022@gmail.com>
2022-10-19 22:17:29 -0300

    adicionando funcionalidade localstorag

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 const form = document.getElementById('formulario');
 const lista = document.getElementById('lista');
+const tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+
+tarefas.forEach( (elemento) => {
+    addItem(elemento)
+});
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault();
     const txt = evento.target.elements['texto'];
+
+    const tarefa = {
+        "tarefa": txt.value
+    }
     
-    addItem(txt);
+    addItem(tarefa);
+    tarefas.push(tarefa);
+
+    localStorage.setItem("tarefas", JSON.stringify(tarefas));
 
     txt.value = '';
 })
@@ -13,8 +25,7 @@ form.addEventListener("submit", (evento) => {
 function addItem (item) {
     const novoItem = document.createElement('li');
     novoItem.classList.add('item');
-    novoItem.innerHTML = item.value;
+    novoItem.innerHTML += item.tarefa;
 
     lista.appendChild(novoItem);
-    console.log(novoItem);
 }
