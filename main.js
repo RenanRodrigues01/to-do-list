19f1c75fa861a98b033efa6ae96ab6fcf59b22af
renan <renan.rodriguesmoura2022@gmail.com>
2022-10-20 21:41:03 -0300

    adicionando funcionalidades js

--- main.js
+++ main.js
@@ -9,13 +9,18 @@ tarefas.forEach( (elemento) => {
 form.addEventListener("submit", (evento) => {
     evento.preventDefault();
     const txt = evento.target.elements['texto'];
-
+    console.log(txt);
     const tarefa = {
         "tarefa": txt.value
     }
-    
-    addItem(tarefa);
-    tarefas.push(tarefa);
+
+    if(txt.value == ''){
+        alert('Adicione uma tarefa')
+    }else{
+        tarefa.id = tarefas[tarefas.length -1] ? (tarefas[tarefas.length -1]).id +1 : 0;
+        addItem(tarefa);
+        tarefas.push(tarefa);
+    }
 
     localStorage.setItem("tarefas", JSON.stringify(tarefas));
 
@@ -27,5 +32,26 @@ function addItem (item) {
     novoItem.classList.add('item');
     novoItem.innerHTML += item.tarefa;
 
+    novoItem.appendChild(deletItem(item.id));
+
     lista.appendChild(novoItem);
+
+}
+
+function deletItem(id) {
+    const botaoX = document.createElement('button');
+    botaoX.innerText = 'X';
+
+    botaoX.addEventListener('click', function() {
+        removeItem(this.parentNode, id)
+    })
+
+    return botaoX;
+}
+
+function removeItem(elemento, id) {
+    elemento.remove();
+    tarefas.splice(tarefas.findIndex(elemento => elemento.id === id) ,1);
+   
+    localStorage.setItem("tarefas", JSON.stringify(tarefas));
 }
